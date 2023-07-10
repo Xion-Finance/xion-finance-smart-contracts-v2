dec0f756fdac798a9c70e914a102480b1eb48772
Aeryn Quarmby <67911815+aerynquarmby@users.noreply.github.com>
2023-07-10 14:55:08 +0200

    Delete IXGHub.sol

--- contracts/interfaces/IXGHub.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0
-pragma solidity 0.7.6;
-
-interface IXGHub {
-    function getAuthorizationStatus(address _address)
-        external
-        view
-        returns (bool);
-}
